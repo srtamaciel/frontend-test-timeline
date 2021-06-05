99e95a6408e466248949763cc1ecb2e7230f9c44
Raquel Rodriguez <srtademaciel@gmail.com>
2021-06-05 20:44:58 +0200

    Add clip, resizable clips and export button working

--- src/index.js
+++ src/index.js
@@ -1,166 +1,100 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {v4 as uuid} from "uuid";
-import {DragDropContext, Droppable, Draggable} from "react-beautiful-dnd";
+import RGL, {WidthProvider} from "react-grid-layout";
 
 import "./index.css";
 
-// This method is needed for rendering clones of draggables
-const getRenderItem = (items, className) => (provided, snapshot, rubric) => {
-  const item = items[rubric.source.index];
-  return (
-    <React.Fragment>
-      <li
-        {...provided.draggableProps}
-        {...provided.dragHandleProps}
-        ref={provided.innerRef}
-        style={provided.draggableProps.style}
-        className={snapshot.isDragging ? "dragging" : ""}
-      >
-        {item.label}
-      </li>
-    </React.Fragment>
-  );
+const ReactGridLayout = WidthProvider(RGL);
+
+let idCounter = 0;
+
+const getId = () => {
+  idCounter++;
+  return `Clip ${idCounter.toString()}`;
 };
 
-const Copyable = (props) => {
-  return (
-    <Droppable
-      renderClone={getRenderItem(props.items, props.className)}
-      droppableId={props.droppableId}
-      isDropDisabled={true}
-    >
-      {(provided, snapshot) => (
-        <ul ref={provided.innerRef} className={props.className}>
-          {props.items.map((item, index) => {
-            const shouldRenderClone = item.id === snapshot.draggingFromThisWith;
-            return (
-              <React.Fragment key={item.id}>
-                {shouldRenderClone ? (
-                  <li className="react-beatiful-dnd-copy">{item.label}</li>
-                ) : (
-                  <Draggable draggableId={item.id} index={index}>
-                    {(provided, snapshot) => (
-                      <React.Fragment>
-                        <li
-                          ref={provided.innerRef}
-                          {...provided.draggableProps}
-                          {...provided.dragHandleProps}
-                          className={snapshot.isDragging ? "dragging" : ""}
-                        >
-                          {item.label}
-                        </li>
-                      </React.Fragment>
-                    )}
-                  </Draggable>
-                )}
-              </React.Fragment>
-            );
-          })}
-          {provided.placeholder}
-        </ul>
-      )}
-    </Droppable>
-  );
-};
+class App extends React.PureComponent {
+  static defaultProps = {
+    isDraggable: true,
+    isResizable: true,
+    items: 5,
+    rowHeight: 30,
+    preventCollision: false,
+    cols: 12,
+  };
 
-const Clips = (props) => {
-  return <Copyable droppableId="CLIPS" className="clips" items={props.items} />;
-};
+  state = {
+    layout: [
+    /*   { x: 0, y: 0, w: 3, h: 3, i: getId() },
+      { x: 0, y: 1, w: 3, h: 3, i: getId() },
+      { x: 0, y: 2, w: 3, h: 3, i: getId() } */
+    ]
+  };
 
+  render() {
+    return (
+      <React.Fragment>
+        <button onClick={this.addNewItem}>Add clip</button>
+        <ReactGridLayout
+          {...this.props}
+          onLayoutChange={(layout) => this.setState({layout})}
+        >
+          {this.state.layout.map((item) => (
+            <div key={item.i} data-grid={item}>
+              <span>{item.i}</span>
+            </div>
+          ))}
+        </ReactGridLayout>
+        <button onClick={this.export}>Export</button>
+      </React.Fragment>
+    );
+  }
+ 
+  addNewItem = () => {
+    const {layout} = this.state;
+    const newItem = {x: 0, y: 0, w: 3, h: 3, i: getId()};
 
-const Timeline = (props) => {
-  console.log(props)
-  return (
-    <Droppable droppableId="TIMELINE">
-      {(provided) => (
-        <div className="container-timeline">
-          <ul id="timeline" ref={provided.innerRef} className="timeline">
-            {props.items.map((item, index) => (
-              <Draggable key={item.id} draggableId={item.id} index={index}>
-                {(provided) => (
-                  <li
-                    id="clip-box"
-                    ref={provided.innerRef}
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}
-                    style={provided.draggableProps.style}
-                  >
-                    {item.label}
-                    {console.log(document.getElementById('timeline').getBoundingClientRect())}
-                  </li>
-                )}
-              </Draggable>
-            ))}
-            {provided.placeholder}
-          </ul>
-        </div>
-      )}
-    </Droppable>
-  );
-};
+    if (layout.some((item) => item.x === 0 && item.y === 0)) {
+      this.setState({
+        layout: layout
+          .map((item) => {
+            if (item.x === 0) {
+              return {y: item.y++, ...item};
+            }
 
+            return item;
+          })
+          .concat([newItem]),
+      });
+    } else {
+      this.setState({layout: layout.concat([newItem])});
+    }
+  };
 
+  export = () => {
+    const unitiesOfTimeline = this.state.layout.map((clip) => {
+      return clip;
+    });
 
-const COLLECTION = [
-  {id: uuid(), label: "Clip 1"},
-  {id: uuid(), label: "Clip 2"},
-  {id: uuid(), label: "Clip 3"},
-];
+    console.log(
+      `In this timeline we have ${
+        this.state.layout.length
+      } clips, each of them have ${unitiesOfTimeline.map(
+        (clip) => clip.y
+      )} axis Y position and takes ${unitiesOfTimeline.map(
+        (clip) => clip.w
+      )} unities of the timeline, a total of ${
+        this.state.layout.reduce((acc, unities) => {
+          return acc + unities.w ;
+        }, 0)
+      } unities. `
+    );
+  };
+}
 
-const reorder = (list, startIndex, endIndex) => {
-  const [removed] = list.splice(startIndex, 1);
-  list.splice(endIndex, 0, removed);
-  return list;
-};
-
-const copy = (source, destination, droppableSource, droppableDestination) => {
-  const item = source[droppableSource.index];
-  destination.splice(droppableDestination.index, 0, {...item, id: uuid()});
-  return destination;
-  
-};
-
-
-const App = () => {
-  const [timelineItems, setTimelineItems] = React.useState([]);
-  const onDragEnd = React.useCallback(
-    (result) => {
-      console.log(result);
-      const {source, destination} = result;
-
-      if (!destination) {
-        return;
-      }
-
-      switch (source.droppableId) {
-        case destination.droppableId:
-          setTimelineItems((state) =>
-            reorder(state, source.index, destination.index)
-          );
-          break;
-        case "CLIPS":
-          setTimelineItems((state) =>
-            copy(COLLECTION, state, source, destination)
-          );
-          break;
-        default:
-          break;
-      }
-    },
-    [setTimelineItems]
-  );
-  return (
-    <div className="App">
-      <DragDropContext onDragEnd={onDragEnd}>
-        <h2>Clips</h2>
-        <Clips items={COLLECTION} />
-        <h2>Time line</h2>
-        <Timeline items={timelineItems} />
-      </DragDropContext>
-    </div>
-  );
-};
+/* if (require.main === module) {
+  require(".../test-hook.jsx")(module.exports);
+} */
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
