ac7870b34ee474d99843b981c07253f975617dfc
Raquel Rodriguez <srtademaciel@gmail.com>
2021-06-07 17:06:29 +0200

    button export shows the message at the console and document - styles

--- src/index.js
+++ src/index.js
@@ -25,34 +25,52 @@ class App extends React.PureComponent {
 
   state = {
     layout: [
-    /*   { x: 0, y: 0, w: 3, h: 3, i: getId() },
+      /*   { x: 0, y: 0, w: 3, h: 3, i: getId() },
       { x: 0, y: 1, w: 3, h: 3, i: getId() },
-      { x: 0, y: 2, w: 3, h: 3, i: getId() } */
-    ]
+      { x: 0, y: 2, w: 3, h: 3, i: getId() }  */
+    ],
+    showMessage: false,
   };
 
   render() {
     return (
       <React.Fragment>
-        <button onClick={this.addNewItem}>Add clip</button>
+        <div className="button">
+          <button onClick={this.addNewItem}>Add clip</button>
+        </div>
+        <div className="timeline-title">
+          <h2>Timeline</h2>
+        </div>
         <ReactGridLayout
+        className="react-layout"
           {...this.props}
           onLayoutChange={(layout) => this.setState({layout})}
         >
+        
           {this.state.layout.map((item) => (
-            <div key={item.i} data-grid={item}>
+            <div
+              key={item.i}
+              data-grid={item}
+            >
               <span>{item.i}</span>
             </div>
           ))}
         </ReactGridLayout>
-        <button onClick={this.export}>Export</button>
+        <div className="button">
+          <button onClick={this.export}>Export</button>
+        </div>
+        {this.state.showMessage && (
+          <div className="export-info">
+            <p>{this.export()}</p>
+          </div>
+        )}
       </React.Fragment>
     );
   }
- 
+
   addNewItem = () => {
     const {layout} = this.state;
-    const newItem = {x: 0, y: 0, w: 3, h: 3, i: getId()};
+    const newItem = {x: 0, y: 0, w: 3, h: 2, i: getId()};
 
     if (layout.some((item) => item.x === 0 && item.y === 0)) {
       this.setState({
@@ -61,7 +79,6 @@ class App extends React.PureComponent {
             if (item.x === 0) {
               return {y: item.y++, ...item};
             }
-
             return item;
           })
           .concat([newItem]),
@@ -75,20 +92,39 @@ class App extends React.PureComponent {
     const unitiesOfTimeline = this.state.layout.map((clip) => {
       return clip;
     });
+    this.setState({showMessage: true});
 
     console.log(
       `In this timeline we have ${
         this.state.layout.length
       } clips, each of them have ${unitiesOfTimeline.map(
         (clip) => clip.y
-      )} axis Y position and takes ${unitiesOfTimeline.map(
+      )} axis Y position and axis X ${unitiesOfTimeline.map(
+        (clip) => clip.x
+      )}. It takes ${unitiesOfTimeline.map(
         (clip) => clip.w
-      )} unities of the timeline, a total of ${
-        this.state.layout.reduce((acc, unities) => {
-          return acc + unities.w ;
-        }, 0)
-      } unities. `
+      )} unities of the timeline, a total of ${this.state.layout.reduce(
+        (acc, unities) => {
+          return acc + unities.w;
+        },
+        0
+      )} unities. `
     );
+
+    return `In this timeline we have ${
+      this.state.layout.length
+    } clips, each of them have ${unitiesOfTimeline.map(
+      (clip) => clip.y
+    )} axis Y position and axis X ${unitiesOfTimeline.map(
+      (clip) => clip.x
+    )}. It takes ${unitiesOfTimeline.map(
+      (clip) => clip.w
+    )} unities of the timeline, a total of ${this.state.layout.reduce(
+      (acc, unities) => {
+        return acc + unities.w;
+      },
+      0
+    )} unities. `;
   };
 }
 
