f8832869d176c8a4d1c26717e52086b73a3ccec5
Raquel Rodriguez <srtademaciel@gmail.com>
2021-06-07 22:38:23 +0200

    buttons for clip 1, clip2 and clip 3. Reorderinginfo export button

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ const ReactGridLayout = WidthProvider(RGL);
 
 let idCounter = 0;
 
-const getId = () => {
+const getId = (num) => {
   idCounter++;
-  return `Clip ${idCounter.toString()}`;
+  return `Clip ${num} ${idCounter.toString()}`;
 };
 
 class App extends React.PureComponent {
@@ -33,44 +33,10 @@ class App extends React.PureComponent {
   };
 
   render() {
-    return (
-      <React.Fragment>
-        <div className="button">
-          <button onClick={this.addNewItem}>Add clip</button>
-        </div>
-        <div className="timeline-title">
-          <h2>Timeline</h2>
-        </div>
-        <ReactGridLayout
-        className="react-layout"
-          {...this.props}
-          onLayoutChange={(layout) => this.setState({layout})}
-        >
-        
-          {this.state.layout.map((item) => (
-            <div
-              key={item.i}
-              data-grid={item}
-            >
-              <span>{item.i}</span>
-            </div>
-          ))}
-        </ReactGridLayout>
-        <div className="button">
-          <button onClick={this.export}>Export</button>
-        </div>
-        {this.state.showMessage && (
-          <div className="export-info">
-            <p>{this.export()}</p>
-          </div>
-        )}
-      </React.Fragment>
-    );
-  }
 
-  addNewItem = () => {
+  const addNewItem1 = () => {
     const {layout} = this.state;
-    const newItem = {x: 0, y: 0, w: 3, h: 2, i: getId()};
+    const newItem = {x: 0, y: 0, w: 3, h: 2, i: getId(1)};
 
     if (layout.some((item) => item.x === 0 && item.y === 0)) {
       this.setState({
@@ -88,49 +54,106 @@ class App extends React.PureComponent {
     }
   };
 
-  export = () => {
-    const unitiesOfTimeline = this.state.layout.map((clip) => {
-      return clip;
-    });
+  const addNewItem2 = () => {
+    const {layout} = this.state;
+    const newItem = {x: 0, y: 0, w: 3, h: 2, i: getId(2)};
+
+    if (layout.some((item) => item.x === 0 && item.y === 0)) {
+      this.setState({
+        layout: layout
+          .map((item) => {
+            if (item.x === 0) {
+              return {y: item.y++, ...item};
+            }
+            return item;
+          })
+          .concat([newItem]),
+      });
+    } else {
+      this.setState({layout: layout.concat([newItem])});
+    }
+  };
+
+  const addNewItem3 = () => {
+    const {layout} = this.state;
+    const newItem = {x: 0, y: 0, w: 3, h: 2, i: getId(3)};
+
+    if (layout.some((item) => item.x === 0 && item.y === 0)) {
+      this.setState({
+        layout: layout
+          .map((item) => {
+            if (item.x === 0) {
+              return {y: item.y++, ...item};
+            }
+            return item;
+          })
+          .concat([newItem]),
+      });
+    } else {
+      this.setState({layout: layout.concat([newItem])});
+    }
+  };
+
+  const unitiesOfTimeline = this.state.layout.map((clip, index) => {
+    return(
+       <div key={index}>{clip.i.slice(0, 6)} in the position X: {clip.x} Y: {clip.y}. It takes {clip.w} units of the timeline</div>
+    )
+  });
+  
+  const exportButton = () => {
     this.setState({showMessage: true});
 
-    console.log(
-      `In this timeline we have ${
-        this.state.layout.length
-      } clips, each of them have ${unitiesOfTimeline.map(
-        (clip) => clip.y
-      )} axis Y position and axis X ${unitiesOfTimeline.map(
-        (clip) => clip.x
-      )}. It takes ${unitiesOfTimeline.map(
-        (clip) => clip.w
-      )} unities of the timeline, a total of ${this.state.layout.reduce(
-        (acc, unities) => {
-          return acc + unities.w;
-        },
-        0
-      )} unities. `
-    );
-
-    return `In this timeline we have ${
+    return <div>In this timeline we have {
       this.state.layout.length
-    } clips, each of them have ${unitiesOfTimeline.map(
-      (clip) => clip.y
-    )} axis Y position and axis X ${unitiesOfTimeline.map(
-      (clip) => clip.x
-    )}. It takes ${unitiesOfTimeline.map(
-      (clip) => clip.w
-    )} unities of the timeline, a total of ${this.state.layout.reduce(
+    } clips: {unitiesOfTimeline} Your timeline have a total of {this.state.layout.reduce(
       (acc, unities) => {
         return acc + unities.w;
       },
       0
-    )} unities. `;
-  };
+    )} units.
+  </div>
+  }
+
+    return (
+      <React.Fragment>
+        <div className="button">
+          <button onClick={addNewItem1}>Add clip 1</button>
+          <button onClick={addNewItem2}>Add clip 2</button>
+          <button onClick={addNewItem3}>Add clip 3</button>
+        </div>
+        <div className="timeline-title">
+          <h2>Timeline</h2>
+        </div>
+        
+        <ReactGridLayout
+        resizeHandles={['e']}
+          {...this.props}
+          onLayoutChange={(layout) => this.setState({layout})}
+        >
+        
+          {this.state.layout.map((item) => (
+            <div
+              key={item.i}
+              data-grid={item}
+            >
+              <span>{item.i.slice(0, 6)}</span>
+            </div>
+          ))}
+        </ReactGridLayout>
+        <div className="button">
+          <button onClick={exportButton}>Export</button>
+        </div>
+        {this.state.showMessage && (
+          <div className="export-info">
+            <div>{exportButton()}</div>
+          </div>
+        )}
+      </React.Fragment>
+    );
+  }
+
 }
 
-/* if (require.main === module) {
-  require(".../test-hook.jsx")(module.exports);
-} */
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
